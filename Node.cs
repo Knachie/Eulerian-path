bca8e526b30117f713761847b6b4c1203ce15746
Kachi Ugwoke <kchugw@gmail.com>
2024-03-23 23:04:19 +0100

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eulerian_path
+{
+    internal class Node
+    {
+        public int name;
+        public int num;
+        private bool isEven;
+
+        public Node(int nodeName, int nodeNum)
+        {
+            name = nodeName;
+            num = nodeNum;
+            if (nodeNum % 2 == 0)
+            {
+                isEven = true;
+            }
+            else
+            {
+                isEven = false;
+            }
+        }
+
+        public bool AskEven()
+        {
+            return isEven;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using ConvertHandler;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Eulerian_path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many nodes does this graph have? ");
+            int length = Handle.Int32(Console.ReadLine());
+            Node[] graph = new Node[length];
+
+            for(int i = 0; i < graph.Length; i++)
+            {
+                Console.Write("How many edges does node " + (i+1) + " have? ");
+                int edgeNum = Handle.Int32(Console.ReadLine());
+                graph[i] = new Node(i + 1, edgeNum);
+            }
+
+            int oddNum = 0;
+
+            for(int i = 0; i < graph.Length; ++i)
+            {
+                if (graph[i].AskEven() == false)
+                {
+                    oddNum++;
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("If a graph is built like this:");
+            WriteArray(graph);
+
+            if (oddNum == 0)
+            {
+                Console.WriteLine("\nAn Eulerian ciciut exists");
+            }
+            else if (oddNum == 2)
+            {
+                Console.WriteLine("\nAn Eulerian path exists");
+            }
+            else
+            {
+                Console.WriteLine("\nNo Eulerian path exists");
+            }
+
+            Task.Delay(5000).Wait();
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+        static void WriteArray(Node[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Node: " + array[i].name + "\t Edges: " + array[i].num);
+            }
+        }
+    }
+}
